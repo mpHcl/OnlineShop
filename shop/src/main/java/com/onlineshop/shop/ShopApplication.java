d38c1d079583d86689f9143c4d63cce15fe75809
Michał Pilch <m.pilch2002@gmail.com>
2020-12-22 20:52:41 +0100

    I created project
    There was no project, and now there is a project

---
+++ shop/src/main/java/com/onlineshop/shop/ShopApplication.java
@@ -0,0 +1,22 @@
+package com.onlineshop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
